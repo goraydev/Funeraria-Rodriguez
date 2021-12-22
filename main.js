94cc81d3cec940bf8d9465f2912b827acac4f646
AYGJ <jofre1236@gmail.com>
2021-12-21 20:26:47 -0500

    Culminando la parte de tipos de ataud y comenzando la parte de arreglos florales

--- main.js
+++ main.js
@@ -1,3 +1,14 @@
+/* --------------- para cambiar el fondo del nav --------------- */
+window.onscroll = function () { scrollFunction() };
+
+function scrollFunction() {
+    if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
+        document.getElementById("nav").style.backgroundColor = "rgba(0, 0, 0, 0.5)";
+    } else {
+        document.getElementById("nav").style.backgroundColor = "rgba(255, 255, 255, 0.15)";
+    }
+}
+
 const nav = document.getElementById("nav");
 const icon = document.getElementById("icon");
 const expand = (e) => {
